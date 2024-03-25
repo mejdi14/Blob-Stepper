1c8cdaf64ec4c380955d886625d403afde120bb0
mejdi14 <mejdiclubiste@gmail.com>
2024-03-25 17:43:15 +0100

    add text data class

--- app/src/main/java/com/example/blob_stepper/MainActivity.kt
+++ app/src/main/java/com/example/blob_stepper/MainActivity.kt
@@ -31,6 +31,8 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
+import com.example.blob_stepper.component.BlobCircleComposable
+import com.example.blob_stepper.component.ProgressCircleComposable
 import com.example.blob_stepper.controller.BlobActionListener
 import com.example.blob_stepper.controller.BlobProgressController
 import com.example.blob_stepper.data.BlobCircle
@@ -67,76 +69,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     BlobStepperTheme {
         Greeting("Android")
-    }
-}
-
-
-@Composable
-fun AnimatedSmoothAlternatingWavesBlob(
-    blobCircle: BlobCircle = BlobCircle(),
-    controller: BlobProgressController,
-    blobActionListener: BlobActionListener
-) {
-    val infiniteTransition = rememberInfiniteTransition(label = "")
-    val animatedWave = infiniteTransition.animateFloat(
-        initialValue = 0f,
-        targetValue = 2f * Math.PI.toFloat(),
-        animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis = 1000, easing = LinearEasing),
-            repeatMode = RepeatMode.Restart
-        ), label = ""
-    )
-
-    val targetRadius = if (controller.isExpanded.value) 250f else 200f
-    val animatedRadius by animateFloatAsState(
-        targetValue = targetRadius,
-        animationSpec = tween(durationMillis = 500, easing = LinearOutSlowInEasing), label = ""
-    )
-
-    Canvas(
-        modifier = Modifier
-            .fillMaxSize()
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onTap = {
-                        if (controller.isExpanded.value) controller.shrink() else controller.next()
-                        blobActionListener.onChangeListener(controller.currentStep.value)
-                    }
-                )
-            }
-    ) {
-        val centerX = size.width / 2
-        val centerY = size.height / 2
-        val path = Path()
-
-        if (controller.isExpanded.value) {
-            path.addOval(
-                androidx.compose.ui.geometry.Rect(
-                    centerX - animatedRadius,
-                    centerY - animatedRadius,
-                    centerX + animatedRadius,
-                    centerY + animatedRadius
-                )
-            )
-        } else {
-            for (angle in 0 until 360 step 5) {
-                val radian = Math.toRadians(angle.toDouble()).toFloat()
-                val phaseShift = Math.PI.toFloat() * (angle / (360f / blobCircle.wavesCount))
-                val waveOffset =
-                    if (!controller.isExpanded.value) 10 * sin(blobCircle.wavesCount * radian + animatedWave.value + phaseShift) else 0f
-                val currentRadius = animatedRadius + waveOffset
-                val x = centerX + currentRadius * cos(radian)
-                val y = centerY + currentRadius * sin(radian)
-
-                if (angle == 0) {
-                    path.moveTo(x, y)
-                } else {
-                    path.lineTo(x, y)
-                }
-            }
-            path.close()
-        }
-        drawPath(path, Color.Black)
     }
 }
 
@@ -179,12 +111,12 @@ private fun BlobStepper(
             .height(300.dp)
             .fillMaxWidth(),
     ) {
-        AnimatedCircularBorderProgress(
+        ProgressCircleComposable(
             progressBorderCircle = progressBorderCircle,
             controller = controller,
             blobActionListener = blobActionListener
         )
-        AnimatedSmoothAlternatingWavesBlob(
+        BlobCircleComposable(
             blobCircle = blobCircle,
             controller = controller,
             blobActionListener = blobActionListener
@@ -192,58 +124,3 @@ private fun BlobStepper(
     }
 }
 
-@Composable
-fun AnimatedCircularBorderProgress(
-    progressBorderCircle: ProgressBorderCircle = ProgressBorderCircle(),
-    controller: BlobProgressController,
-    blobActionListener: BlobActionListener
-) {
-    val animatedProgress by animateFloatAsState(
-        targetValue = controller.progress.value,
-        animationSpec = tween(durationMillis = progressBorderCircle.progressAnimationDurationMillis),
-        label = ""
-    )
-
-    LaunchedEffect(animatedProgress) {
-        if (animatedProgress == controller.progress.value) {
-            if (controller.progress.value == 1f) {
-                controller.completionListener.onProgressCompleted()
-                blobActionListener.onFinishListener()
-            }
-        }
-    }
-
-    Canvas(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        val diameter = progressBorderCircle.diameter
-        val topLeft = Offset(
-            (size.width - diameter) / 2,
-            (size.height - diameter) / 2
-        )
-        val size = Size(diameter, diameter)
-        val startAngle = progressBorderCircle.startAngle.value
-        val sweepAngle = 360 * animatedProgress
-
-        drawArc(
-            color = progressBorderCircle.strokeDefaultColor,
-            startAngle = 0f,
-            sweepAngle = 360f,
-            useCenter = false,
-            topLeft = topLeft,
-            size = size,
-            style = Stroke(width = progressBorderCircle.strokeWidth)
-        )
-
-        drawArc(
-            color = progressBorderCircle.strokeFilledColor,
-            startAngle = startAngle,
-            sweepAngle = sweepAngle,
-            useCenter = false,
-            topLeft = topLeft,
-            size = size,
-            style = Stroke(width = progressBorderCircle.strokeWidth, cap = StrokeCap.Round)
-        )
-    }
-}
